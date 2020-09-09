d6d56a7527b67a71ffaa8091015af393a184c612
15153246027 <652992429@qq.com>
2020-09-09 09:52:23 +0800

    优化response

--- src/routes/uploadFile.js
+++ src/routes/uploadFile.js
@@ -33,27 +33,30 @@ router.post('/save', async function(ctx, next) {
  */
 router.post('/upload', async function(ctx, next) {
   console.log('进入uploadFile');
-  responseFormat.success(ctx, '操作成功');
-  // 上传单个文件
-  // const file = ctx.request.files.file; // 获取上传文件
-  // // 创建可读流
-  // const reader = fs.createReadStream(file.path);
-  // const fileName = new Date().getTime() + file.name;
-  // // const dir = path.join(__dirname, '../../../files/');
-  // const dir = process.env.NODE_ENV ? '/Users/liuyin/fileStore/files' : '/project/fileStore';
-  // const isExist = fs.existsSync(dir);
-  // if (!isExist) {
-  //   await fs.mkdirSync(dir);
-  // }
-  //
-  // const filePath = dir + `/${fileName}`;
-  // // 创建可写流
-  // const upStream = fs.createWriteStream(filePath);
-  // // 可读流通过管道写入可写流
-  // reader.pipe(upStream);
-  // const fileFolder = process.env.NODE_ENV === 'development' ? 'http://localhost/file/' : 'http://www.8000cloud.com/file/';
-  //
-  // responseFormat.success(ctx, '上传成功', { filePath: fileFolder + fileName },);
+  try {
+    // 上传单个文件
+    const file = ctx.request.files.file; // 获取上传文件
+    // 创建可读流
+    const reader = fs.createReadStream(file.path);
+    const fileName = new Date().getTime() + file.name;
+    // const dir = path.join(__dirname, '../../../files/');
+    const dir = process.env.NODE_ENV ? '/Users/liuyin/fileStore/files' : '/project/fileStore';
+    const isExist = fs.existsSync(dir);
+    if (!isExist) {
+      await fs.mkdirSync(dir);
+    }
+
+    const filePath = dir + `/${fileName}`;
+    // 创建可写流
+    const upStream = fs.createWriteStream(filePath);
+    // 可读流通过管道写入可写流
+    reader.pipe(upStream);
+    const fileFolder = process.env.NODE_ENV === 'development' ? 'http://localhost/file/' : 'http://www.8000cloud.com/file/';
+
+    responseFormat.success(ctx, '上传成功', { filePath: fileFolder + fileName });
+  } catch (e) {
+    responseFormat.error(ctx, '上传失败', e);
+  }
 });
 
 module.exports = router;
